e600a5c49d7968fad8cc9bb3fb4666f493795691
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2022-10-17 11:51:08 +0200

    Started with terrain tile blending

--- empires.c
+++ empires.c
@@ -17,11 +17,11 @@
 #define PERLIN_DEFINE
 #include "perlin.h"
 
-typedef struct Object {
+typedef struct Unit {
     int32_t id;
-    int32_t x;
-    int32_t y;
-} Object;
+    float x;
+    float y;
+} Unit;
 
 int main(int argc, char **argv) {
     (void)argc;
@@ -54,8 +54,14 @@ int main(int argc, char **argv) {
     // Terrain.drs
     char terrain_path[255];
     strcpy(terrain_path, root_path);
-    strcat(terrain_path, "/data/terrain.drs");
+    strcat(terrain_path, "/data/Terrain.drs");
     DRS *terrain_drs = drs_new_from_file(terrain_path);
+
+    // Border.drs
+    char border_path[255];
+    strcpy(border_path, root_path);
+    strcat(border_path, "/data/Border.drs");
+    DRS *border_drs = drs_new_from_file(border_path);
 
     // Pallet
     char *palette_text = drs_read_file(interface_drs, DRS_TABLE_BIN, 50500, NULL);
@@ -70,6 +76,13 @@ int main(int argc, char **argv) {
     slp_header *terrain_slps[4];
     for (int32_t i = 0; i < 4; i++) {
         terrain_slps[i] = drs_read_file(terrain_drs, DRS_TABLE_SLP, 15000 + i, NULL);
+    }
+
+    // Load border SLP's
+    // 0 sea sand borders, 1 sand grass borders
+    slp_header *border_slps[6];
+    for (int32_t i = 0; i < 6; i++) {
+        border_slps[i] = drs_read_file(border_drs, DRS_TABLE_SLP, 20000 + i, NULL);
     }
 
     // Load unit SLP's
@@ -87,15 +100,15 @@ int main(int argc, char **argv) {
     // Camera state
     int32_t camera_x = 0;
     int32_t camera_y = 0;
-    bool mouse_down = false;
+    bool camera_drag = false;
 
     // Map generation
     int32_t map_width = 64;
     int32_t map_height = 64;
-    uint8_t map[map_height * map_width];
+    uint16_t map[map_height * map_width];
 
-    size_t objects_size = 0;
-    Object objects[1024];
+    size_t units_size = 0;
+    Unit units[1024];
 
     srand(1);
     perlin_init(rand());
@@ -104,43 +117,49 @@ int main(int argc, char **argv) {
         for (int32_t x = 0; x < map_width; x++) {
             double n = perlin_noise((double)x / 50.0, (double)y / 50.0, 0);
 
-            if (n > 0) map[y * map_width + x] = 1;
-            else if (n > -0.25) map[y * map_width + x] = 0;
-            else if (n > -0.5) map[y * map_width + x] = 2;
-            else map[y * map_width + x] = 3;
+            if (n > 0)
+                map[y * map_width + x] = (1 << 8) | rand() % 9;
+            else if (n > -0.25)
+                map[y * map_width + x] = (0 << 8) | rand() % 9;
+            else if (n > -0.5)
+                map[y * map_width + x] = (2 << 8) | rand() % 4;
+            else
+                map[y * map_width + x] = (3 << 8) | rand() % 4;
 
             if (n > 0.3 && rand() % 2 == 1) {
-                objects[objects_size++] = (Object){.id = 503 + rand() % 4, .x = x, .y = y};
+                units[units_size++] = (Unit){.id = 503 + rand() % 4, .x = x, .y = y};
             }
         }
     }
 
     // Buildings
-    objects[objects_size++] = (Object){.id = 19, .x = 2, .y = 1};
-    objects[objects_size++] = (Object){.id = 117, .x = 5, .y = 5};
-    objects[objects_size++] = (Object){.id = 447, .x = 6, .y = 10};
+    units[units_size++] = (Unit){.id = 19, .x = 2, .y = 1};
+    units[units_size++] = (Unit){.id = 117, .x = 5, .y = 5};
+    units[units_size++] = (Unit){.id = 447, .x = 6, .y = 10};
 
     // Unit
-    objects[objects_size++] = (Object){.id = 442, .x = 12, .y = 13};
+    units[units_size++] = (Unit){.id = 442, .x = 12, .y = 13};
+    units[units_size++] = (Unit){.id = 443, .x = 12.5, .y = 13.2};
+    units[units_size++] = (Unit){.id = 421, .x = 10, .y = 13.2};
 
     // Event loop
     bool running = false;
     while (!running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_MOUSEBUTTONDOWN) {
-                mouse_down = true;
+            if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_RIGHT) {
+                camera_drag = true;
                 break;
             }
             if (event.type == SDL_MOUSEMOTION) {
-                if (mouse_down) {
+                if (camera_drag) {
                     camera_x += event.motion.xrel;
                     camera_y += event.motion.yrel;
                 }
                 break;
             }
             if (event.type == SDL_MOUSEBUTTONUP) {
-                mouse_down = false;
+                camera_drag = false;
                 break;
             }
 
@@ -169,8 +188,18 @@ int main(int argc, char **argv) {
         // Draw map
         for (int32_t y = 0; y < map_height; y++) {
             for (int32_t x = 0; x < map_width; x++) {
-                slp_header *slp = terrain_slps[map[y * map_width + x]];
-                slp_frame *frame = (slp_frame *)((uint8_t *)slp + sizeof(slp_header));
+                uint8_t terrain_type = map[y * map_width + x] >> 8;
+                uint8_t terrain_version = map[y * map_width + x] & 0xff;
+
+                slp_header *slp;
+                slp_frame *frame;
+                if (map[y * map_width + (x - 1)] >> 8 == 0 && map[y * map_width + x] >> 8 == 1) {
+                    slp = border_slps[1];
+                    frame = (slp_frame *)((uint8_t *)slp + sizeof(slp_header) + 2 * sizeof(slp_frame));
+                } else {
+                    slp = terrain_slps[terrain_type];
+                    frame = (slp_frame *)((uint8_t *)slp + sizeof(slp_header) + terrain_version * sizeof(slp_frame));
+                }
 
                 framebuffer_draw_slp(framebuffer, slp, frame,
                                      start_x - terrain_hwidth + x * terrain_hwidth - y * terrain_hwidth,
@@ -185,15 +214,16 @@ int main(int argc, char **argv) {
             }
         }
 
-        // Draw objects
-        for (size_t i = 0; i < objects_size; i++) {
-            Object *object = &objects[i];
-            slp_header *slp = unit_slps[object->id];
+        // Draw units
+        for (size_t i = 0; i < units_size; i++) {
+            Unit *unit = &units[i];
+            slp_header *slp = unit_slps[unit->id];
             slp_frame *frame = (slp_frame *)((uint8_t *)slp + sizeof(slp_header));
             framebuffer_draw_slp(
                 framebuffer, slp, frame,
-                start_x - frame->center_x + object->x * terrain_hwidth - object->y * terrain_hwidth,
-                start_y - frame->center_y + terrain_hheight + object->x * terrain_hheight + object->y * terrain_hheight,
+                start_x - frame->center_x + (int32_t)(unit->x * terrain_hwidth) - (int32_t)(unit->y * terrain_hwidth),
+                start_y - frame->center_y + terrain_hheight + (int32_t)(unit->x * terrain_hheight) +
+                    (int32_t)(unit->y * terrain_hheight),
                 palette);
         }
 
@@ -203,6 +233,9 @@ int main(int argc, char **argv) {
                              palette);
 
         slp_frame *hud_bottom_frame = (slp_frame *)((uint8_t *)hud_slp + sizeof(slp_header) + 1 * sizeof(slp_frame));
+        framebuffer_fill_rect(framebuffer, (framebuffer->width - hud_bottom_frame->width) / 2,
+                              framebuffer->height - hud_bottom_frame->height, hud_bottom_frame->width,
+                              hud_bottom_frame->height, 0x000000);
         framebuffer_draw_slp(framebuffer, hud_slp, hud_bottom_frame, (framebuffer->width - hud_bottom_frame->width) / 2,
                              framebuffer->height - hud_bottom_frame->height, palette);
 
